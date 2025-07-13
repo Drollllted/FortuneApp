063db32cf0c1d2e3395907b22831d438fb4cc491
Artem Tarasov <artemdrolllted@gmail.com>
2025-07-13 18:29:58 +0300

    Initial Commit

---
+++ FortuneApp/FortuneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneAppApp.swift
+//  FortuneApp
+//
+//  Created by Drolllted on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FortuneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
